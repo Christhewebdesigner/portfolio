0d11ee3603db8a0a6ec99bbd6ae341b5242796c1
mistahc <mistahc@hotmail.com>
2018-03-05 14:58:39 +0100

    first animations CV working

--- scripts/index.js
+++ scripts/index.js
@@ -176,6 +176,8 @@ $(document).ready(function() {
         $('#leftPage, #homePage, #rightPage').addClass('showHomePage');
         $('#leftPage, #homePage, #rightPage').removeClass('showRightPage');
         $('#leftPage, #homePage, #rightPage').removeClass('showLeftPage');
+        $('#rightPage article').css({'overflow' : 'scroll'});
+
       })
 
       //---------------------------------------------------ALL PAGES---------------------------------------------------------------
@@ -183,8 +185,8 @@ $(document).ready(function() {
       //PAGE SLIDE SETTINGS
 
       //temporary
-      // $('#leftPage, #homePage, #rightPage').addClass('showRightPage');
-      // $('#leftPage, #homePage, #rightPage').removeClass('showHomePage');
+      $('#leftPage, #homePage, #rightPage').addClass('showLeftPage');
+      $('#leftPage, #homePage, #rightPage').removeClass('showHomePage');
 
       //ON SWIPE
 
@@ -197,16 +199,19 @@ $(document).ready(function() {
       $('#rightPage').on("swiperight", function() {
         $('#leftPage, #homePage, #rightPage').addClass('showHomePage');
         $('#leftPage, #homePage, #rightPage').removeClass('showRightPage');
+        $('#rightPage article').css({'overflow' : 'scroll'});
       });
 
       $('#homePage').on("swiperight", function() {
         $('#leftPage, #homePage, #rightPage').addClass('showLeftPage');
         $('#leftPage, #homePage, #rightPage').removeClass('showHomePage');
+        $('article').css({'overflow' : 'hidden'});
       });
 
       $('#leftPage').on("swipeleft", function() {
         $('#leftPage, #homePage, #rightPage').addClass('showHomePage');
         $('#leftPage, #homePage, #rightPage').removeClass('showLeftPage');
+        $('#rightPage article').css({'overflow' : 'scroll'});
       });
 
       // $(window).on("swiperight", function() {
@@ -220,6 +225,7 @@ $(document).ready(function() {
       $('.squareLittle').eq(1).click(function(){
         $('#leftPage, #homePage, #rightPage').addClass('showLeftPage');
         $('#leftPage, #homePage, #rightPage').removeClass('showHomePage');
+        $('article').css({'overflow' : 'hidden'});
       });
 
       $('.squareLittle').eq(2).click(function(){
@@ -266,61 +272,61 @@ $(document).ready(function() {
 
       var lastScrollTop = 0;
 
-      // $("#homePage article").on("scroll", function(event){
-      //    var st = $(this).scrollTop();
-      //    // var op = 1 - (st * 0.01);
-      //    var opMin = 1 - (st * 0.02);
-      //    var opPlus = st * 0.01;
-      //    var scaleDown = 0.9 -(st/1000);
-      //    var scrollTransform = 'rotate(45deg) scale(' + scaleDown  + ')';
-      //    var marginInfo = 190 / st;
-      //
-      //
-      //    // var squareTop = $('.squareLittle').eq(0).offset().top;
-      //    // var squareBottom = squareTop + $('.squareLittle').eq(0).height(); //300
-      //    // alert(st);//40 per downarrow, key or click // 728 per click on scrollarea // unknown on drag scrollbar
-      //    if(st < 190){  //on this point it is out of the window
-      //
-      //             $('.squareLittle:first-child').animate({'top': '-' + st, 'opacity' : opMin}, 0);
-      //             $('.squareLittle:nth-child(2)').animate({'left': '-' + st, 'opacity' : opMin}, 0);
-      //             $('.squareLittle:nth-child(3)').animate({'right': '-' + st, 'opacity' : opMin}, 0);
-      //             $('.squareLittle:nth-child(4)').animate({'bottom': '-' + st, 'opacity' : opMin}, 0);
-      //
-      //             $('#menuIcon').animate({'opacity' : opPlus}, 0);
-      //             $('.logobar--bottom').animate({'opacity' : opMin}, 0);
-      //             $('#whois').animate({'opacity' : opPlus, 'marginTop' :  marginInfo}, 0);
-      //
-      //
-      //             // if (st > 25){
-      //             //   $('#whois').animate({'opacity' : 1, 'marginTop' :  0}, 1500);
-      //             // }
-      //
-      //             if(st < 120){
-      //               $('#squareBig').css({'transform' : scrollTransform, '-webkit-transform' : scrollTransform, '-moz-transform' : scrollTransform, '-ms-transform' : scrollTransform, '-o-transform' : scrollTransform});
-      //             }
-      //
-      //             if(st < 10){
-      //               $('.squareLittle').css({'border' : '1px solid transparent'});
-      //
-      //               $('.squareLittle').eq(1).css({'border-top' : '1px solid white'});
-      //               $('.squareLittle').eq(2).css({'border-left' : '1px solid white'});
-      //               $('.squareLittle').eq(3).css({'border-top' : '1px solid white', 'border-left' : '1px solid white'});
-      //
-      //             } else{
-      //                 $('.squareLittle').css({'border' : 'none'});
-      //             }
-      //
-      //      lastScrollTop = st;
-      //    }
-      //
-      //    //THIS MAKES IT ALL KINDA CHOPPY, NEEDS A FIX
-      //    else if( st > 190){
-      //        $('#menuIcon').css({'opacity' : 1});
-      //        $('.logobar--bottom').css({'opacity' : 0});
-      //        $('#whois').css({'marginTop' : 0, 'opacity' :  1});
-      //    }
-      //
-      // });
+      $("#homePage article").on("scroll", function(event){
+         var st = $(this).scrollTop();
+         // var op = 1 - (st * 0.01);
+         var opMin = 1 - (st * 0.02);
+         var opPlus = st * 0.01;
+         var scaleDown = 0.9 -(st/1000);
+         var scrollTransform = 'rotate(45deg) scale(' + scaleDown  + ')';
+         var marginInfo = 190 / st;
+
+
+         // var squareTop = $('.squareLittle').eq(0).offset().top;
+         // var squareBottom = squareTop + $('.squareLittle').eq(0).height(); //300
+         // alert(st);//40 per downarrow, key or click // 728 per click on scrollarea // unknown on drag scrollbar
+         if(st < 190){  //on this point it is out of the window
+
+                  $('.squareLittle:first-child').animate({'top': '-' + st, 'opacity' : opMin}, 0);
+                  $('.squareLittle:nth-child(2)').animate({'left': '-' + st, 'opacity' : opMin}, 0);
+                  $('.squareLittle:nth-child(3)').animate({'right': '-' + st, 'opacity' : opMin}, 0);
+                  $('.squareLittle:nth-child(4)').animate({'bottom': '-' + st, 'opacity' : opMin}, 0);
+
+                  $('#menuIcon').animate({'opacity' : opPlus}, 0);
+                  $('.logobar--bottom').animate({'opacity' : opMin}, 0);
+                  $('#whois').animate({'opacity' : opPlus, 'marginTop' :  marginInfo}, 0);
+
+
+                  // if (st > 25){
+                  //   $('#whois').animate({'opacity' : 1, 'marginTop' :  0}, 1500);
+                  // }
+
+                  if(st < 120){
+                    $('#squareBig').css({'transform' : scrollTransform, '-webkit-transform' : scrollTransform, '-moz-transform' : scrollTransform, '-ms-transform' : scrollTransform, '-o-transform' : scrollTransform});
+                  }
+
+                  if(st < 10){
+                    $('.squareLittle').css({'border' : '1px solid transparent'});
+
+                    $('.squareLittle').eq(1).css({'border-top' : '1px solid white'});
+                    $('.squareLittle').eq(2).css({'border-left' : '1px solid white'});
+                    $('.squareLittle').eq(3).css({'border-top' : '1px solid white', 'border-left' : '1px solid white'});
+
+                  } else{
+                      $('.squareLittle').css({'border' : 'none'});
+                  }
+
+           lastScrollTop = st;
+         }
+
+         //THIS MAKES IT ALL KINDA CHOPPY, NEEDS A FIX
+         else if( st > 190){
+             $('#menuIcon').css({'opacity' : 1});
+             $('.logobar--bottom').css({'opacity' : 0});
+             $('#whois').css({'marginTop' : 0, 'opacity' :  1});
+         }
+
+      });
 
 
 
